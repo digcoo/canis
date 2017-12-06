7b7b4fd5989742821d54c703916754d459de20bd
Ubuntu <ubuntu@ip-172-31-12-247.cn-north-1.compute.internal>
2017-12-06 19:12:26 +0000

    udpate

--- CorpLiepinSpider.py
+++ CorpLiepinSpider.py
@@ -1,17 +1,17 @@
 #encoding=utf-8
 import os
 import traceback
+import urllib
 import urllib2
 from urllib2 import Request
 from bs4 import BeautifulSoup
 import time
 import jsonpickle
-
+from MysqlClient import *
 
 def get_industry_category_urls():
 
     try:
-
 
         category_urls = []
 
@@ -31,15 +31,16 @@ def get_industry_corp_list(base_url):
 	page = 0
 	url = base_url.format(page)
 	corps = get_page_corp_list(url)
+	print "collect base_url = {0}, page = {1}, corp_num = {2}".format(base_url, page, len(corps))
 	while corps is not None and len(corps) > 0:
 	    #process corps persist
+	    MysqlClient.get_instance().add_batch_corps(corps)
 	    print "collect base_url = {0}, page = {1}, corp_num = {2}".format(base_url, page, len(corps))
+	    time.sleep(1)
 
 	    page += 1
 	    url = base_url.format(page)
 	    corps = get_page_corp_list(url)
-	    break
-
 
     except Exception, e:
 	traceback.print_exc()
@@ -47,13 +48,16 @@ def get_industry_corp_list(base_url):
 def get_page_corp_list(url):
     try:
 	html = urllib2.urlopen(url)
+	print html.read()
         bsObj = BeautifulSoup(html,"lxml")
         items = bsObj.findAll("a",{"class":"logo-box"})
 	corps = []
+	print 'items_size = ' + str(len(items))
 	if items is not None and len(items) > 0:
 	    for item in items:
 		corp_info = get_corp_info(item["href"])
 		corps.append(corp_info)
+		time.sleep(2)
 	return corps
 
     except Exception, e:
@@ -67,17 +71,39 @@ def get_corp_info(url):
 	corp_name = bsObj.find("div",{"class":"name-and-welfare"}).find("h1").text.strip()
 	corp_name = corp_name[:len(corp_name) - 5]
 	corp_logo = bsObj.find("img",{"class":"bigELogo"})["src"]
-	corp_summary = bsObj.find("p",{"class":"profile"}).text.strip()
+	corp_follow = get_follow_number(url)
+	corp_summary = ''
+	if bsObj.find("p",{"class":"profile"}) is not None:
+	    corp_summary = bsObj.find("p",{"class":"profile"}).text.strip()[:2000]
+
+	corp_industry_text = bsObj.find("ul",{"class":"new-compintro"}).findAll("li")[0].text
+	corp_industry  = corp_industry_text[corp_industry_text.find(u'：')+1:]
+	corp_addr = bsObj.find("ul",{"class":"new-compintro"}).findAll("li")[2]["title"]
 
 	print "corp_name:\t"  + corp_name
 	print "corp_logo:\t"  + corp_logo
+	print "corp_follow:\t"  + str(corp_follow)
+	print "corp_industry:\t"  + corp_industry
+	print "corp_addr:\t"  + corp_addr
 #	print "corp_summary:\t"  + corp_summary
 	print "*" * 120
 
-	return {"corp_name":corp_name, "corp_logo":corp_logo, "corp_summary":corp_summary}
+	return {"name":corp_name, "logo":corp_logo, "follow":corp_follow, "industry":corp_industry,"addr":corp_addr, "summary":corp_summary}
 
     except Exception, e:
 	traceback.print_exc()
+	return False
+
+def get_follow_number(corp_base_url):
+    follow_base_url = 'https://c.liepin.com/connection/loadattention-b.json'
+    corp_base_url = corp_base_url[:len(corp_base_url) - 1]
+    corp_id = corp_base_url[corp_base_url.rfind("/")+1:]
+    form_map = {'userh_ids':corp_id}
+    head_map = {'X-Requested-With' : 'XMLHttpRequest'}
+    data = urllib.urlencode(form_map)
+    req = urllib2.Request(follow_base_url, headers=head_map, data=data)
+    json_data = urllib2.urlopen(req).read()
+    return jsonpickle.decode(json_data)['data']['attentions'][0]['attention_cnt']
 
 
 def download_logo(local_filename, img_url):
@@ -98,6 +124,5 @@ category_urls = get_industry_category_urls()
 for category_url in category_urls:
     print category_url
     get_industry_corp_list(category_url)
-    break
-
-#get_corp_info('https://www.liepin.com/company/1145474/')
+#get_corp_info('https://www.liepin.com/company/8405646/')
+#get_follow_number('https://www.liepin.com/company/2174886/')
---
+++ MysqlClient.py
@@ -0,0 +1,160 @@
+#encoding=utf-8
+
+import pymysql.cursors
+import datetime
+import time
+import traceback
+import jsonpickle
+
+class MysqlClient:
+
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if MysqlClient.__instance is None:
+            MysqlClient.__instance = MysqlClient()
+        return MysqlClient.__instance
+
+    def __init__(self):
+	self.connection = None
+
+    def conn(self):
+        try:
+            self.connection = pymysql.connect(host='cms-metadata-dev-117.cdslstjgevzs.rds.cn-north-1.amazonaws.com.cn',
+                             user='bestv',
+                             password='bestvwin',
+                             db='anas',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+        except Exception, e:
+            traceback.print_exc()
+
+    def close(self):
+	try:
+#	    self.connection.close()
+	    print ''
+	except Exception, e:
+	    traceback.print_exc()
+
+#//////////////////////////////////////////////////////////////////university/////////////////////////////////////////////////////////////////////////
+
+
+    def add_university(self, university):
+        try:
+            local_universities = self.query_university_by_name(university['local_name'])
+	    if local_universities == None or len(local_universities) == 0:
+		self.conn()
+                with self.connection.cursor() as cursor:
+                    sql = "INSERT INTO `digcoo_anas_base_university` (`local_name`, `cn_name`, `nation`, `gmt_created`, `gmt_updated`, `del`) values(%s, %s, '中国', now(), now(), 0)"
+		    cursor.execute(sql, (university['local_name'], university['cn_name']))
+                    self.connection.commit()
+
+        except Exception, e:    
+	    traceback.print_exc()
+	finally:
+	    self.close()
+
+
+
+    def query_university_by_name(self, name):
+        try:
+            self.conn()
+            with self.connection.cursor() as cursor:
+                sql = "SELECT `id`,`local_name`, `cn_name` from `digcoo_anas_base_university` where `local_name` = %s "
+                cursor.execute(sql, (name, ))
+                return cursor.fetchall()
+        except Exception, e:
+            traceback.print_exc()
+        return None
+
+
+
+#//////////////////////////////////////////////////////////////////corp/////////////////////////////////////////////////////////////////////////
+
+
+    def add_corp(self, corp):
+        try:
+            local_corps = self.query_corp_by_name(corp['name'])
+            if local_corps == None or len(local_corps) == 0:
+                self.conn()
+                with self.connection.cursor() as cursor:
+                    sql = "INSERT INTO `digcoo_anas_base_corp` (`name`, `logo`, `follow`, `industry`, `addr`, `summary`, `gmt_created`, `gmt_updated`, `del`) values(%s, %s, %s, %s, %s, %s, now(), now(), 0)"
+                    cursor.execute(sql, (corp['name'], corp['logo'], corp['follow'], corp['industry'], corp['addr'], corp['summary']))
+                    self.connection.commit()
+
+        except Exception, e:
+            traceback.print_exc()
+        finally:
+            self.close()
+
+
+
+    def query_corp_by_name(self, name):
+        try:
+            self.conn()
+            with self.connection.cursor() as cursor:
+                sql = "SELECT `id`,`name`, `logo`, `follow` from `digcoo_anas_base_corp` where `name` = %s "
+                cursor.execute(sql, (name, ))
+                return cursor.fetchall()
+        except Exception, e:
+            traceback.print_exc()
+        return None
+
+
+    def add_batch_corps(self, corps):
+        try:
+	    for corp in corps:
+		self.add_corp(corp)
+        except Exception, e:
+            traceback.print_exc()
+        finally:
+            self.close()
+
+#//////////////////////////////////////////////////////////////////corp/////////////////////////////////////////////////////////////////////////
+
+
+
+#//////////////////////////////////////////////////////////////////hr_corp/////////////////////////////////////////////////////////////////////////
+
+    def add_hr_corp(self, corp):
+        try:
+            local_corps = self.query_hr_corp_by_name(corp['name'])
+            if local_corps == None or len(local_corps) == 0:
+                self.conn()
+                with self.connection.cursor() as cursor: 
+                    sql = "INSERT INTO `digcoo_anas_base_hr_corp` (`name`, `rigister_info`, `gmt_created`, `gmt_updated`, `del`) values(%s, %s, now(), now(), 0)"
+                    cursor.execute(sql, (corp['name'], corp['rigister_info']))
+                    self.connection.commit()
+                
+        except Exception, e:
+            traceback.print_exc()
+        finally:    
+            self.close()
+
+                        
+    def query_hr_corp_by_name(self, name):
+        try:        
+            self.conn() 
+            with self.connection.cursor() as cursor:
+		sql = "SELECT `id`, `name`, `tel`, `email`, `addr` from `digcoo_anas_base_hr_corp` where `name` = %s "
+                cursor.execute(sql, (name, ))
+                self.connection.commit()
+                
+        except Exception, e:
+            traceback.print_exc()
+        finally:
+            self.close()
+                        
+                
+    def add_batch_hr_corps(self, corps):
+        try:
+            for corp in corps:
+                self.add_hr_corp(corp)
+        except Exception, e:
+            traceback.print_exc()
+        finally:
+	    self.close()
+
+#//////////////////////////////////////////////////////////////////hr_corp/////////////////////////////////////////////////////////////////////////
+
